a69171999cb9a15efe39f387f783e3766403c784
bayarens <bay.arens@gmail.com>
2021-06-26 13:54:46 -0400

    links updated

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 export default function NavBar(props){
     return(
         <div id='navBar'>
-            <Link to='/'> <button className="button">Home</button></Link>
-            <Link to='/movies'> <button className="button">Movies</button></Link>
-            <Link to='/tv'><button className="button">TV</button></Link>
+            <Link to='/nexflix-replica/'> <button className="button">Home</button></Link>
+            <Link to='/nexflix-replica/movies'> <button className="button">Movies</button></Link>
+            <Link to='/nexflix-replica/tv'><button className="button">TV</button></Link>
         </div>
     )
 }
